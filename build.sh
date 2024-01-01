9101baf9841b0586bdbf3f524e60e4444ca80317
Ross Wintle <rosswintle@users.noreply.github.com>
2024-01-01 10:09:10 +0000

    Remove pagefind from Git and add a deploy script

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 php mpress scrape
+# Also in deploy script as we don't commit the results
 npx pagefind
 npx tailwindcss -i ./resources/css/styles.css -o ./public/css/styles.css
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Also in deploy script as we don't commit the results
+npx pagefind
